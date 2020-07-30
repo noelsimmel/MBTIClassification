92a634dfa9d0546b033741f93076ffc6f7588d0f
Noel <simmel@uni-potsdam.de>
2020-07-30 17:11:59 +0200

    Befülle main-Datei, schreibe Docstring. Entferne preprocessing

--- main.py
+++ main.py
@@ -4,24 +4,39 @@
 # Abgabe: 31.08.20
 
 import sys
-from preprocessing import preprocess
 from MBTIClassifier import MBTIClassifier
 from MBTIClassifierTrain import MBTIClassifierTrain 
 
 def main(args):
+    '''
+    Main-Funktion. 
+    Trainiert den Klassifikator oder benutzt ihn für die Klassifikation. \n
+
+    **Parameter**: \n
+    args (list): Argumentliste aus der Kommandozeile. 
+        1. Name der json-Datei mit den Twitter-Daten im Format des TwiSty-Korpus'.
+        2. Name der tsv-Datei, in die die Features gespeichert werden sollen 
+        (im Training) bzw. aus der sie ausgelesen werden sollen (bei der Inferenz).
+        3. (optional) Die Flag -t startet den Trainingsmodus. Ohne sie wird der 
+        Inferenzmodus gestartet.
+    '''
+
     data_filename = args[1]
     feature_filename = args[2]
     print(data_filename, feature_filename)
     
     # Training
-    if argc == 4 and args[3] == '-t':
+    if len(args) == 4 and args[3] == '-t':
         print("TRAINING")
-        pass
+        clf = MBTIClassifierTrain(data_filename, feature_filename)
+        clf.train()
+        clf.evaluate()
 
     # Inferenz
     else:
         print("INFERENZ")
-        pass
+        clf = MBTIClassifier(data_filename, feature_filename)
+        clf.predict()
 
 
 if __name__ == '__main__':
