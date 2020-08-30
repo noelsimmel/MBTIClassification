a36f47c5c28ccd11ccccd499d7f5ba8f7a4b1629
Noel <simmel@uni-potsdam.de>
2020-08-30 18:00:26 +0200

    Tests beschreiben

--- test_module.py
+++ test_module.py
@@ -14,7 +14,8 @@ logging.disable(logging.CRITICAL)
 
 class TestClassifier(unittest.TestCase):
     '''
-    Test-Klasse für den MBTI-Klassifikator.
+    Test-Klasse für den MBTI-Klassifikator. 
+    Aufruf mit python -m unittest test_module.
     '''
     def setUp(self):
         self.clf = MBTIClassifier()
@@ -26,10 +27,12 @@ class TestClassifier(unittest.TestCase):
         self.assertEqual(tweet.text, "Twitter is about to go offline.  See you soon!")
 
     def test_2_data_load_from_json(self):
+        # Daten aus einer json-Datei in einen DataFrame laden
         df = self.clf._preprocess(self.data)
         self.assertIs(type(df), pd.DataFrame)
 
     def test_3_dataset_split(self):
+        # Datenset aufsplitten
         train_data, val_data, test_data = self.clf.split_dataset('data/TwiSty-DE.json')
         os.remove('data/dataset_training.json')
         os.remove('data/dataset_validation.json')
@@ -37,24 +40,30 @@ class TestClassifier(unittest.TestCase):
         self.assertTrue(len(train_data) > len(test_data) > len(val_data))
 
     def test_4_feature_extraction(self):
+        # Daten einlesen und Features extrahieren
         df = self.clf._preprocess(self.data)
         features = self.clf._extract_features(df)
         self.assertEqual(features.shape, (14, 24)) # 14 Instanzen
 
     def test_5_training(self):
+        # Modell trainieren
         aggregated_features = self.clf.train(self.data, 'unittest.tsv')
         # Datenset enthält 8 verschiedene Klassen
         self.assertEqual(len(aggregated_features), 8) # 8 Klassen
 
     def test_6_predicting(self):
-        predictions = self.clf.predict(self.data, 'unittest.tsv')
+        # Modell benutzen um Vorhersagen zu treffen
+        # Aus Einfachheitsgründen Trainingsset = Eingabedaten
+        predictions = self.clf.predict(self.data, 'unittest.tsv', 'unittest_predictions.tsv')
         self.assertEqual(predictions.shape, (14, 4)) # 14 Instanzen
 
     def test_7_evaluating(self):
+        # Accuracy evaluieren
         # Aus Einfachheitsgründen Trainingsset = Testset
         accuracy = self.clf.evaluate(self.data, 'unittest.tsv')
         os.remove('unittest.tsv')
-        os.remove('predictions.tsv')
+        os.remove('unittest_predictions.tsv')
+        os.remove('gold_predictions.tsv')
         self.assertTrue(accuracy > 0.0)
 
 
